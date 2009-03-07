12bc05d8c00f6c09284b079c65bcc81ffd7e4758
stephenmg12 <stephenmg12@392b6082-0498-11de-b4d5-6978306beb92>
2009-03-07 22:38:25 +0000

    class.killlist.php - changed the way the sql query is built home.php - added needed options to support new properties in class.killlist.php

--- branches/dev/class/class.killlist.php
+++ branches/dev/class/class.killlist.php
@@ -31,7 +31,6 @@
  * @link       http://www.eve-online.com/
  */
 
-
 /**
  * killList
  * 
@@ -41,20 +40,53 @@
  * @version $Id$
  * @access public
  */
-class killList 
-{   
+class killList
+{
     /**
      * 
      * @var object
      */
     private $rs;
-    
+
     /**
      * 
      * @var array
      */
     public $rarray;
+
+    public $fetchAlliance = false;
+
+    public $fetchCorp = false;
+
+    public $fetchFaction = false;
     
+    public $fetchWeek=true;
+
+    public $corpID = 0;
+
+    public $allianceID = 0;
+
+    public $factionID = 0;
+
+    public $week = NULL; // 1-53
+
+    public $year = NULL;
+
+    public $countInvolved = false;
+
+    public $filterClass = null;
+
+    function __construct()
+    {
+        $this->SQL_start = 'SELECT  it.typeName as shiptype, cv.characterName as victimName, cv.corporationName as vcorpName, cv.allianceName as valliName, map.solarSystemName, map.security, caf.characterName as killerName, caf.corporationName AS kcorpName, caf.allianceName AS kalliNmae,it.graphicID, kl.killTime, kl.killID';
+        $this->SQL_joins = ' FROM `corpKillLog` kl' .
+            ' JOIN `corpVictim` cv ON cv.`killID`=kl.`killID`' .
+            ' JOIN `corpAttackers` ca ON ca.`killID`=cv.`killID`' .
+            ' JOIN `corpAttackers` caf ON caf.`killID`=cv.`killID`' .
+            ' JOIN `invTypes` it ON it.`typeID` = cv.`shipTypeID`' .
+            ' JOIN `mapSolarSystems` map ON map.`solarSystemID` = kl.`solarSystemID`';
+        $this->SQL_end = ' ORDER BY kl.`killTime` DESC';
+    }
 
     /**
      * killList::fetchList()
@@ -67,47 +99,82 @@ class killList
      * @param mixed $year
      * @return void
      */
-    function fetchList($ID=KKS_KBCORPID, $isAlliance=FALSE,$week=NULL, $year=NULL) {
-            //Get ADODB Factory INSTANCE
-            $instance = ADOdbFactory::getInstance();
-            //Get DB Connection
-            $con = $instance->factory(KKS_DSN);
-            
-            if(!is_int($ID)) {
-                $ID=KKS_KBCORPID;            
-            }
-            if(!is_int($week) && $week<=0 || $week>=53) {
-                $week='WEEK(NOW())';   
-            }
-            if(!is_int($year)) {
-                $year='YEAR(NOW())';            
-            }
-            if($isAlliance){
-                $WHERE=' ca.allianceID='.$ID.'';
-            }
-            else {
-                $WHERE=' ca.corporationID='.$ID.'';
-            }
-            
-            $sql = 'SELECT  it.typeName as shiptype, cv.characterName as victimName, cv.corporationName as vcorpName, cv.allianceName as valliName, map.solarSystemName, map.security,'
+    function fetchList()
+    {
+        $con = $this->get_connection();
+
+        /*
+        $sql = 'SELECT  it.typeName as shiptype, cv.characterName as victimName, cv.corporationName as vcorpName, cv.allianceName as valliName, map.solarSystemName, map.security,'
         .' caf.characterName as killerName, caf.corporationName AS kcorpName, caf.allianceName AS kalliNmae,it.graphicID, kl.killTime, kl.killID'
         .' FROM `corpKillLog` kl'
-        .' JOIN `corpVictim` cv ON cv.`killID`=kl.`killID`'
+        .' JOIN `corpVictim` cv ON cv.`killID`=kl.`killID`' 
         .' JOIN `corpAttackers` ca ON ca.`killID`=cv.`killID`'
         .' JOIN `corpAttackers` caf ON caf.`killID`=cv.`killID`'
         .' JOIN `invTypes` it ON it.`typeID` = cv.`shipTypeID`'
         .' JOIN `mapSolarSystems` map ON map.`solarSystemID` = kl.`solarSystemID`'
-        .' WHERE '.$WHERE.' '
+        .' WHERE '.$WHERE.' '//TODO:Move to new system
         .' AND caf.`finalBlow`=1' 
-        .' AND WEEK( kl.`killTime` ) = '.$week.' AND YEAR(kl. `killTime`)='.$year
-        .' ORDER BY kl.`killTime` DESC';     
+        .' AND WEEK( kl.`killTime` ) = '.$week.' AND YEAR(kl. `killTime`)='.$year//TODO:move to new system
+        .' ORDER BY kl.`killTime` DESC';
+        */
 
-            if($this->rs=$con->CacheExecute(KKS_CACHE_KILLLIST, $sql)){
-            	$this->rarray=$this->rs->GetAssoc();
-            } else {
-            	trigger_error('SQL Query Failed', E_USER_ERROR);
-            }
-            
+        //Change Week and Year to format we can use
+        if(!isset($this->week) && !is_numeric($this->week)) {
+            $this->week = date('W');
+        }
+        if(!isset($this->year) && !is_numeric($this->year)) {
+            $this->year = date('Y');
+        }
+        if($this->week<10) {
+            $week=$this->week;
+            $pad=str_pad($week, 2, 0, STR_PAD_LEFT);
+            $this->week=substr($pad, -2, 2);
+        }
+        $start_date=date( 'Y-m-d H:i:s', strtotime($this->year.'W'.$this->week));
+        $end_date=date( 'Y-m-d H:i:s', strtotime($this->year.'W'.$this->week.'7 23 hour 59 minutes 59 seconds'));
+        
+        $sql = $this->SQL_start;
+        $sql .= $this->SQL_joins;
+        $sql .= ' WHERE caf.`finalBlow`=1';
+        if ($this->fetchCorp == true && $this->corpID > 0)
+        {
+            $sql .= ' AND ca.corporationID=' . $this->corpID;
+        }
+        if ($this->fetchAlliance == true && $this->allianceID > 0)
+        {
+            $sql .= ' AND ca.allianceID=' . $this->allianceID;
+        }
+        if ($this->fetchFaction == true && $this->factionID > 0)
+        {
+            $sql .= ' AND ca.factionID=' . $this->factionID;
+        }
+        if($this->fetchWeek == true) {
+            $sql .= ' AND kl.`killTime`BETWEEN "'.$start_date.'" AND "'.$end_date.'"';    
+        }
+        $sql .= $this->SQL_end;
+
+        if($this->rs=$con->CacheExecute(KKS_CACHE_KILLLIST, $sql)){
+        $this->rarray=$this->rs->GetAssoc();
+        } else {
+        trigger_error('SQL Query Failed', E_USER_ERROR);
+        } 
+
+    }
+
+    /**
+     * killList::get_connection()
+     * 
+     * connection function // returns active connection or establishes new sql con
+     * 
+     * @return $con
+     */
+    function get_connection()
+    {
+        //Get ADODB Factory INSTANCE
+        $instance = ADOdbFactory::getInstance();
+        //Get DB Connection
+        $con = $instance->factory(KKS_DSN);
+        return $con;
     }
 }
 
